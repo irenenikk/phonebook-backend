6ff62bf048cafe33fbe0d5ef92c7f3646023aa7e
Irene <irene.nikkarinen@helsinki.fi>
2018-01-02 14:00:14 +0200

    Add simple info page (ex 41)

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  const date = new Date()
+  const totalPersons = persons.length
+  const response = `<div>Puhelinluettelossa on ${totalPersons} ihmisen tiedot</div>`.concat(`<div>${date}</div>`)
+  res.send(response)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
